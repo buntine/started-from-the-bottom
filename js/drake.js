c8bd4d8ca137f71c475c1a01e144c50ed91f6ac0
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-08-08 17:45:31 +1000

    Added progress calculation

--- js/drake.js
+++ js/drake.js
@@ -1,7 +1,26 @@
 (function(){
-  var noggun = document.getElementById("drake");
+  var noggun, totalHeight;
 
-  document.addEventListener("load", function(){
-    console.log("ww");
+  var position = function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+        progress = (scrollTop / (totalHeight - window.innerHeight));
+    
+    return progress;
+  };
+
+  window.addEventListener("load", function(){
+    var body = document.body,
+        html = document.documentElement;
+
+    noggun = document.getElementById("drake");
+    totalHeight = Math.max(body.scrollHeight, body.offsetHeight, 
+                    html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+    noggun.style.top = window.innerHeight + "px";
+  });
+
+  window.addEventListener("scroll", function(){
+
+    console.log(position());
   });
 })();
